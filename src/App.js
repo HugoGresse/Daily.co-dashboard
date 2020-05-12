ec25bd2634731080a1d888d6f551b40ab3081181
Hugo Gresse <hugo.gresse@gmail.com>
2020-05-12 11:05:47 +0200

    Remove nused parameter

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     const dispatch = useDispatch()
 
     useEffect(() => {
-        dispatch(getMeetings(false))
+        dispatch(getMeetings())
     }, [dispatch])
 
     return (
